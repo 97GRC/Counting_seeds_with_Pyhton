0667e8fe3c4a0c4735bd5cc5c83daa91dc9d9f2b
97GRC <campos.gab@usp.br>
2020-12-01 02:08:23 +0000

    second

--- onlyOne.py
+++ onlyOne.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
-import cv2                          # package to image manipulation
+#Bibliotecas
+import cv2                          
 import numpy as np
+import sys
+import os
+import pandas as pd
+import re
 
-#########
 #FUNÇÃO
-img = cv2.imread('avc.jpg') 
+files = 'feijao_001.jpg' 
 
-def seeds_number(img):
+def seeds_number(files):
+	img = cv2.imread(files)
 	blur = cv2.blur(img, (50, 50))
 	grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-	th = cv2.threshold(grey, 60, 255, cv2.THRESH_BINARY)[1]
+	th = cv2.threshold(grey, 110, 255, cv2.THRESH_BINARY)[1]
 	sp = cv2.morphologyEx(th,
                        cv2.MORPH_OPEN,
                        np.ones((10, 10), np.uint8),
@@ -20,23 +25,52 @@ def seeds_number(img):
                                        cv2.CHAIN_APPROX_SIMPLE)
 	return len(contours)
 
-print(seeds_number(img))
+
+#LENDO VÁRIOS ARQUIVOS 
+os.chdir(sys.argv[1]) #Verificar se só tem imagens e como o nome eh
+files = os.listdir()
+files.sort()
+print(files)
+value = 0
+
+num_seeds = []
+
+for i in files:
+	imagem = files[value]
+	value += 1	
+	num_seeds.append(seeds_number(imagem))
+	
+print(num_seeds)
+
+#CRIANDO ARQUIVO CSV
+trat = []
+value = 0
+
+for i in files:
+	names = files[value]
+	value += 1
+	match = re.findall(r'^\w.+_', names)
+	trat.append(match)
+print(trat)
+
+Id = []
+value = 0
+
+for i in files:
+	ID = files[value]
+	value += 1
+	match2 = re.findall(r'\d\d\d', ID)
+	Id.append(match2)
+print(Id) 
+
+#SALVANDO OS DADOS NUM ARQUIVO CSV
+df = pd.DataFrame(data={'Trat': trat, 'Id': Id, 'num_seeds': num_seeds})
+df.to_csv("./Seeds.csv", sep = ',', index = False)
+
+#ANÁLISE EXPLORATÓRIA
+
+	
 
 
 
-###########
-img = cv2.imread('avc.jpg')
 
-blur = cv2.blur(img, (50, 50))
-grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-th = cv2.threshold(grey, 60, 255, cv2.THRESH_BINARY)[1]
-sp = cv2.morphologyEx(th,
-                       cv2.MORPH_OPEN,
-                       np.ones((10, 10), np.uint8),
-                       iterations = 4)  
-
-contours, hierarchy = cv2.findContours(th,
-                                       cv2.RETR_EXTERNAL, 
-                                       cv2.CHAIN_APPROX_SIMPLE)
-
-#print(len(contours))
